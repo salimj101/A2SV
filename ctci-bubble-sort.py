8962394cf560dd655ce338744b58ef9c506e51a5
Salim Jibril <salimjibril560@gmail.com>
2024-05-04 10:23:29 +0300

    insertionsort1

--- ctci-bubble-sort.py
+++ ctci-bubble-sort.py
@@ -12,18 +12,20 @@ import sys
 # The function accepts INTEGER_ARRAY a as parameter.
 #
 
+
 def countSwaps(a):
     # Write your code here
-    count=0
+    count = 0
     for j in range(n):
-        for i in range(1,n):
-            if a[i]< a[i-1]:
-                a[i],a[i-1]= a[i-1],a[i]
-                count+=1
+        for i in range(1, n):
+            if a[i] < a[i-1]:
+                a[i], a[i-1] = a[i-1], a[i]
+                count += 1
     print("Array is sorted in " + str(count) + " swaps.")
     print("First Element: " + str(a[0]))
     print("Last Element: " + str(a[-1]))
-            
+
+
 if __name__ == '__main__':
     n = int(input().strip())
 
