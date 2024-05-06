2f43eb7497b602af24a7652e1d07ce7f55bdc6a9
Salim Jibril <salimjibril560@gmail.com>
2024-05-06 15:16:02 +0300

    Remove Smallest

---
+++ Remove Smallest.py
@@ -0,0 +1,11 @@
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    arr = list(map(int, input().split()))
+    arr.sort()
+    for i in range(1, n):
+        if abs(arr[i] - arr[i - 1]) > 1:
+            print("NO")
+            break
+    else:
+        print("YES")
