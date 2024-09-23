9b3ee50b30737946faa1d475fde836a3a8665de2
Salim Jibril <salimjibril560@gmail.com>
2024-09-23 12:05:18 +0300

    solved

---
+++ merge-sorted-array.py
@@ -0,0 +1,10 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        l=m
+        for i in range(n):
+           nums1[l]=nums2[i]
+           l+=1
+        nums1.sort()
