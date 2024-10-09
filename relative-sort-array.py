740a24ca74ae4758d59811e737ad605c5c620758
Salim Jibril <salimjibril560@gmail.com>
2024-10-09 12:05:18 +0300

    solved

---
+++ relative-sort-array.py
@@ -0,0 +1,13 @@
+class Solution:
+    def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        ans=[]
+        for i in arr2:
+            for j in range(len(arr1)):
+                if i == arr1[j]:
+                    ans.append(arr1[j])
+                    arr1[j]= -1
+        arr1.sort()
+        for i in arr1:
+            if i != -1:
+                ans.append(i)
+        return ans
