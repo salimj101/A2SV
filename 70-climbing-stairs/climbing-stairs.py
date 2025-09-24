da8bbf3bf2a8aa8ec2f8ef6c87d281ed9a761153
salimj101 <138696471+salimj101@users.noreply.github.com>
2025-09-24 17:27:07 +0300

    Time: 0 ms (100.00%) | Memory: 18 MB (8.35%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.py
@@ -0,0 +1,18 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        memo = defaultdict(int)
+        
+        def dp(n):
+            if n in memo:
+                return memo[n]
+            if n == 1:
+                return 1
+            if n == 2:
+                return 2
+            
+            memo[n] = dp(n-1) + dp(n-2)
+            return dp(n-1) + dp(n-2)
+
+        return dp(n)
+
+        
