98efab5c68b348e93390fdf0d6d677a609efc611
salimj101 <138696471+salimj101@users.noreply.github.com>
2025-10-07 10:32:40 +0300

    Time: 671 ms (7.91%) | Memory: 201.5 MB (17.83%) - LeetSync

---
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maxProfit(self, prices: List[int], fee: int) -> int:
+        
+        n = len(prices)
+        memo = {}
+        def dp(i,holding):
+            if i == n:
+                return 0
+
+            if (i, holding) in memo:
+                return memo[(i, holding)]
+
+            profit = dp(i+1, holding)
+
+            if holding:
+                profit = max(profit, prices[i]- fee + dp(i+1,0))
+
+            else:
+                profit= max(profit, -prices[i]+dp(i+1,1))
+
+            memo[(i, holding)] = profit
+            return profit
+
+        return dp(0,0)
